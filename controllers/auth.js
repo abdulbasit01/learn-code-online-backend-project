98fe1d062b046ff92f8cb215a524a68d89622d4d
abdul basit <31815370+abdulbasit01@users.noreply.github.com>
2022-04-17 07:55:02 -0700

    token creation signup api

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,10 @@
 const User = require('../models/user')
+var jwt = require('jsonwebtoken');
+var expressJwt = require('express-jwt');
+var cookieParser = require('cookie-parser')
 
 exports.signout = (req, res, next) => {
+  res.clearCookie('token')
   res.json({
     message: "User is signout"
   })
@@ -10,9 +14,34 @@ exports.signout = (req, res, next) => {
 
 exports.signup = (req, res) => {
   const user = new User(req.body)
-  console.log(user)
   user.save((error, user) => {
     if (!error) return res.status(200).json(user)
     return res.status(400).json(error)
+  })
+}
+
+exports.signin = (req, res, next) => {
+  const { email, password } = req.body
+  User.findOne({ email }, (error, user) => {
+    if (error || !user) {
+      return res.status(400).json({
+        error: "USER email does not exists"
+      });
+    }
+
+    if (!user.authenticate(password)) {
+      return res.status(401).json({
+        error: "Email and password do not match"
+      });
+    }
+    // put token in cookies
+    const { _id, name, email, purchases, role } = user
+    // create token
+    const token = jwt.sign({ id: _id, _id, name, email, purchases, role }, process.env.SECRET)
+    res.cookie('token', token, { expire: new Date() + 999 })
+    res.json({
+      status: 200,
+      token
+    })
   })
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,20 @@
 var router = require("express").Router();
 
-const { signout, signup } = require('../controllers/auth')
+const { signout, signup, signin } = require('../controllers/auth')
 const { body, validationResult } = require('express-validator');
 
 router.post(
   "/signup",
   body('name', 'name must be of 3 characters').isLength({ min: 3 }),
+  body('email', 'Email is empty').notEmpty(),
   body('email', 'Email is not correct format').isEmail(),
   body('password', 'password must be greater than 5 characters').isLength({ min: 5 }),
+  body('confirm_password').custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error('Password confirmation does not match password');
+    }
+    return true
+  }),
   (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -17,6 +24,22 @@ router.post(
   }
   ,
   signup);
+
+
+router.post(
+  '/signin',
+  body('email', "Email is empty, please enter email").notEmpty(),
+  body('email', "Email is not correct in the format").isEmail(),
+  body('password', "password must must be equal or greater than 4 characters").isLength({ min: 4 }),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array().map(({ location, ...rErrors }) => rErrors) });
+    }
+    next()
+  },
+  signin
+)
 router.get("/signout", signout);
 
 module.exports = router;
